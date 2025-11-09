a7b20f37aed730562fb228602d3727ab5026f59d
Yeo <yeosuwacos@gmail.com>
2025-11-09 16:04:52 -0500

    Ver 0.2
    Core loop almost complete, only requires selling minigames and more workshop minigames + some ar assets

---
+++ chimata-project 0.2/scenes/game/selling_system.gd
@@ -0,0 +1,76 @@
+extends Node2D
+
+#Refreshes the card labels
+func _ready():
+	$CardSale/Labels/Xs.text = "Lesser ability cards " + str(Global.sold_xs) + "/" + str(Global.ability_card_xs)
+	$CardSale/Labels/S.text = "Ability cards " + str(Global.sold_s) + "/" + str(Global.ability_card_s)
+	$CardSale/Labels/M.text = "Greater ability cards " + str(Global.sold_m) + "/" + str(Global.ability_card_m)
+	$CardSale/Labels/L.text = "Special ability cards " + str(Global.sold_l) + "/" + str(Global.ability_card_l)
+	$CardSale/Labels/Xl.text = "Quest ability cards " + str(Global.sold_xl) + "/" + str(Global.ability_card_xl)
+
+#Adding or removing cards for sale
+
+#Xs
+func _on_add_xs_pressed() -> void:
+	if Global.sold_xs < Global.ability_card_xs:
+		Global.sold_xs += 1
+		$CardSale/Labels/Xs.text = "Lesser ability cards " + str(Global.sold_xs) + "/" + str(Global.ability_card_xs)
+
+func _on_rem_xs_pressed() -> void:
+	if Global.sold_xs > 0:
+		Global.sold_xs -= 1
+		$CardSale/Labels/Xs.text = "Lesser ability cards " + str(Global.sold_xs) + "/" + str(Global.ability_card_xs)
+
+#S
+func _on_add_s_pressed() -> void:
+	if Global.sold_s < Global.ability_card_s:
+		Global.sold_s += 1
+		$CardSale/Labels/S.text = "Ability cards " + str(Global.sold_s) + "/" + str(Global.ability_card_s)
+
+func _on_rem_s_pressed() -> void:
+	if Global.sold_s > 0:
+		Global.sold_s -= 1
+		$CardSale/Labels/S.text = "Ability cards " + str(Global.sold_s) + "/" + str(Global.ability_card_s)
+
+#M
+func _on_add_m_pressed() -> void:
+	if Global.sold_m < Global.ability_card_m:
+		Global.sold_m += 1
+		$CardSale/Labels/M.text = "Greater ability cards " + str(Global.sold_m) + "/" + str(Global.ability_card_m)
+
+func _on_rem_m_pressed() -> void:
+	if Global.sold_m > 0:
+		Global.sold_m -= 1
+		$CardSale/Labels/M.text = "Greater ability cards " + str(Global.sold_m) + "/" + str(Global.ability_card_m)
+
+#L
+func _on_add_l_pressed() -> void:
+	if Global.sold_l < Global.ability_card_l:
+		Global.sold_l += 1
+		$CardSale/Labels/L.text = "Special ability cards " + str(Global.sold_l) + "/" + str(Global.ability_card_l)
+
+func _on_rem_l_pressed() -> void:
+	if Global.sold_l > 0:
+		Global.sold_l -= 1
+		$CardSale/Labels/L.text = "Quest ability cards " + str(Global.sold_l) + "/" + str(Global.ability_card_l)
+
+#Xl
+func _on_add_xl_pressed() -> void:
+	if Global.sold_xl < Global.ability_card_xl:
+		Global.sold_xl += 1
+		$CardSale/Labels/Xl.text = "Special ability cards " + str(Global.sold_xl) + "/" + str(Global.ability_card_xl)
+
+func _on_rem_xl_pressed() -> void:
+	if Global.sold_xl > 0:
+		Global.sold_xl -= 1
+		$CardSale/Labels/Xl.text = "Special ability cards " + str(Global.sold_xl) + "/" + str(Global.ability_card_xl)
+
+#Selling the desired cards without bartering
+func _on_sell_pressed() -> void:
+	var total = 0
+	total += (Global.sold_xs * 50 + Global.sold_s * 300 + Global.sold_m * 825 + Global.sold_l * 2150 + Global.sold_xl * 5400)
+	sell(total)
+
+#Confirming a sale	
+func sell(total):
+	Global.funds += total
---
+++ chimata-project 0.2/scenes/global script/global.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+#Mine minigame flags
+var isMining = false
+var moves = 20
+
+#Chimata camera flag
+var follow = false
+
+#Workshop minigame flags
+var game1Running = false
+
+#Cards to be sold
+var sold_xs = 0
+var sold_s = 0
+var sold_m = 0
+var sold_l = 0
+var sold_xl = 0
+
+#Mining resources
+var dragon_gem_xs = 0
+var dragon_gem_s = 0
+var dragon_gem_m = 0
+var dragon_gem_l = 0
+var dragon_gem_xl = 0
+
+#Workshop resources
+var ability_card_xs = 9
+var ability_card_s = 9
+var ability_card_m = 9
+var ability_card_l = 9
+var ability_card_xl = 9
+
+#Currencies
+var funds = 0
