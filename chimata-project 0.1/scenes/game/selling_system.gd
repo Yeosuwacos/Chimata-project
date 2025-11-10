ce5407b096794a716bceb3eda8f040456abc091e
Yeosuwacos <yeosuwacos@gmail.com>
2025-11-09 22:40:06 -0500

    File reorganization

--- chimata-project 0.1/scenes/game/selling_system.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-#Refreshes the card labels
-func _ready():
-	$CardSale/Labels/Xs.text += " " + str(Global.sold_xs) + "/" + str(Global.ability_card_xs)
-	$CardSale/Labels/S.text += " " + str(Global.sold_s) + "/" + str(Global.ability_card_s)
-	$CardSale/Labels/M.text += " " + str(Global.sold_m) + "/" + str(Global.ability_card_m)
-	$CardSale/Labels/L.text += " " + str(Global.sold_l) + "/" + str(Global.ability_card_l)
-	$CardSale/Labels/Xl.text += " " + str(Global.sold_xl) + "/" + str(Global.ability_card_xl)
